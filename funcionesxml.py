56c7dfbda6c011006a1a41b367fff43e6c3c0884
juanlu-millan <juanlumillanhidalgo@gmail.com>
2019-03-24 21:47:09 +0100

    funciones

--- funcionesxml.py
+++ funcionesxml.py
@@ -9,3 +9,11 @@ def provincias(doc):
 def radares(doc):
     lista = doc.xpath("count(//RADAR)")
     return lista
+
+    #Pedir por teclado una provincia y mostrar el nombre de las carreteras que tiene y la cantidad de radares.
+
+def poblacion(provincia,doc):
+    carretera = doc.xpath('//PROVINCIA[NOMBRE="%s"]/./CARRETERA/DENOMINACION/text()'%provincia)
+    radares = int(doc.xpath('count(//PROVINCIA[NOMBRE="%s"]/./CARRETERA/RADAR)'%provincia))
+    info = [carretera,radares]
+    return info
